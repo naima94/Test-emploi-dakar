48318323b6beae24b9a2d5585088ec458a3b1dd0
naima94 <naimatou.adambi@gmail.com>
2021-10-09 06:59:28 -0800

    Contain & should

--- cypress/integration/1st-test.js
+++ cypress/integration/1st-test.js
@@ -1,10 +1,18 @@
 /// <reference types="cypress" />
 
-it('google test', () => {
+
+
+it('emploi-dakar test', () => {
     cy.visit('https://www.emploidakar.com/')
     cy.get('.text-inner > .button').click()
     cy.get('#menu-item-125914 > .nav-top-link').click()
     cy.get('li.job_listing').first().click()
+    cy.contains('Postuler')
+        .should('contain', 'Postuler')
+        .should('be.visible')
 
+    cy.get('.application_button').click()
+
+    cy.go('back')
 
 })
