586bfc84972240a98221e3a070a4a5369a77c846
naima94 <naimatou.adambi@gmail.com>
2021-10-23 14:48:14 -0800

    Comment

---
+++ cypress/integration/1st-test.spec.js
@@ -0,0 +1,18 @@
+/// <reference types="cypress" />
+
+
+
+describe('emploi-dakar test', () => {
+    it('Job offer test', () => {
+        // Go to the web site home page
+        cy.visit('https://www.emploidakar.com/')
+
+        cy.get('.text-inner > .button').click()
+        cy.get('#menu-item-125914 > .nav-top-link').click()
+        cy.get('li.job_listing').first().click()
+        cy.contains('Postuler')
+            .should('contain', 'Postuler')
+            .should('be.visible')
+    })
+
+})
