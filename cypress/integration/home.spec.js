9f949aa266da19d38aca4d6795e29a0be673a874
naima94 <naimatou.adambi@gmail.com>
2021-10-25 14:33:03 -0800

    Header V2

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,6 +1,7 @@
 /// <reference types="cypress" />
 
 describe('Home page UI test', () => {
+    // ---------------- HEADER TEST ----------------
     it('Load home page', () => {
         cy.visit('https://www.emploidakar.com/')
     })
@@ -27,18 +28,59 @@ describe('Home page UI test', () => {
     it('Search onglet visibility', () => {
         cy.get('.is-small > .icon-search').should('be.visible')
         cy.get('.is-small > .icon-search').realHover()
-        //cy.get('.nav-dropdown .nav-dropdown-default').should('be.visible')
-        //cy.get('.search-field').first().should('be.visible')
+        cy.get('.search-field').first().should('be.visible')
     })
+
+    // ----------- NAV MENU -----------
+
+    it('Nav menu visibility', () => {
+        cy.contains('Offres d’Emploi').should('be.visible')
+    })
+
+    it('Recruters visibility', () => {
+        cy.contains('Recruteurs').should('be.visible')
+        //Recruteurs hover
+        cy.get('#menu-item-11059 > .nav-top-link').realHover()
+        cy.contains('Publier une Offre').should('be.visible')
+        cy.contains('Gérer mes offres').should('be.visible')
+        cy.contains('Accéder à la CVthèque').should('be.visible')
+        cy.contains('Packs & Tarifs de publication').should('be.visible')
+    })
+
+    it('Candidats visibility', () => {
+        cy.contains('Candidats').should('be.visible')
+        //Candidats hover
+        cy.get('#menu-item-699 > .nav-top-link').realHover()
+        cy.contains('Modèles de CV').should('be.visible')
+        cy.contains('Lettres de Motivation').should('be.visible')
+        cy.contains('Mes Alertes Emploi').should('be.visible')
+        cy.contains('Actualités Emploi').should('be.visible')
+        cy.contains('Candidature Spontanée').should('be.visible')
+        cy.contains('Bourses & Concours').should('be.visible')
+        cy.contains('Entrepreneuriat').should('be.visible')
+        cy.contains('Recrutement Afrique').should('be.visible')
+    })
+
+    it('Trouver une Formation visibility', () => {
+        cy.contains('Trouver une Formation').should('be.visible')
+        //Trouver une Formation hover
+        cy.get('#menu-item-133975 > .nav-top-link').realHover()
+        cy.contains('Formations en Français').should('be.visible')
+        cy.contains('Free Online Courses').should('be.visible')
+        cy.contains('Certifications').should('be.visible')
+    })
+
+    it('Publier une Offre d’Emploi visibility', () => {
+        cy.contains('Publier une Offre d’Emploi').should('be.visible')
+        //Publier une Offre d’Emploi hover
+        //cy.get('.header-nav > .header-button-2 > .header-button > .button').first().realHover()
+        //.should('have.css', 'color', 'rgb(0,115,177)') //'
+    })
+
 
     it(' visibility', () => {
 
     })
-
-    it(' visibility', () => {
-
-    })
-
 
 
 })
