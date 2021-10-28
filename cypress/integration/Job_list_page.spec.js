e26dfd12c428fe8fcdba5ba38591f4fe91b9a8db
naima94 <naimatou.adambi@gmail.com>
2021-10-28 15:36:55 -0800

    Filters

--- cypress/integration/Job_list_page.spec.js
+++ cypress/integration/Job_list_page.spec.js
@@ -24,41 +24,67 @@ describe('Jobs list page test', () => {
             .should('have.text', "Toutes les Offres d’Emploi à Dakar et partout au Sénégal")
     })
 
-
-
-
-    //Check if at least 1 job is posted
-    it('At least 1 job exist', () => {
-        cy.get('.post-146656 > a').should('be.visible')
+    //Check key words filter
+    it('Key words filter', () => {
+        cy.get('#search_keywords').should('be.visible')
+            .type('developpeur{enter}')
     })
 
-    //Check if this job title is visible
-    it('Job title is visible', () => {
-        cy.get('.post-146656 > a > .position > h3').should('be.visible')
+
+
+
+    //Return to job list page
+    it('Reset page link', () => {
+
+        cy.get('body')
+            .then(($body) => {
+                const result = cy.get('.job_filters > span').should('include', 'Recherche terminée.')
+
+                // If result found, Reset page 
+                if (result = true) {
+                    cy.get('.reset').click()
+                    cy.url().should('eq', 'https://www.emploidakar.com/offres-demploi-au-senegal/')
+                }
+
+                // else (no result found), Got to job list page
+                else {
+                    cy.get('#menu-item-125914 > .nav-top-link').click()
+                }
+            })
+    })
+
+    //Check if at least 1 job is posted
+    it('At least 1 job is posted', () => {
+        cy.get('.job_listings> li').should('be.visible')
+    })
+
+    //Check if the 1rst job title is visible
+    it('1rst Job title is visible', () => {
+        cy.get('.job_listings> li .position > h3').should('be.visible')
     })
 
     //Check if company name is visible
     it('Company name is visible', () => {
-        cy.get('.post-146656 > a > .position > .company > strong').should('be.visible')
+        cy.get('.job_listings> li .company').first().should('be.visible')
     })
 
     //Check if company logo is visible
     it('Company logo is visible', () => {
-        cy.get('.post-146656 > a > .company_logo').should('be.visible')
+        cy.get('.job_listings> li .company_logo').first().should('be.visible')
     })
 
     //Check if job location is visible
     it('Job location is visible', () => {
-        cy.get('.post-146656 > a > .location').should('be.visible')
+        cy.get('.job_listings> li .location').first().should('be.visible')
     })
 
     //Check if job type is visible
     it('Job type is visible', () => {
-        cy.get('.post-146656 > a > .meta > .job-type').should('be.visible')
+        cy.get('.job_listings> li .job-type').first().should('be.visible')
     })
 
     //Check if job publish date is visible
     it('Job publish date is visible', () => {
-        cy.get('.post-146656 > a > .meta > .date > time').should('be.visible')
+        cy.get('.job_listings> li .date > time').first().should('be.visible')
     })
 })
